0c50fc89b5803ce39a3e2defbdb072f4e6ed6b16
DarkoGNU <42816979+DarkoGNU@users.noreply.github.com>
2021-12-15 13:19:37 +0100

    Project - initial work on interpreting

--- Interpreter/Interpreter.cpp
+++ Interpreter/Interpreter.cpp
@@ -1,1 +1,8 @@
 #include "Interpreter.h"
+
+Interpreter::Interpreter(Params &params)
+    : params(params), tilemap(params.getSize()), turtle(tilemap) {}
+
+bool Interpreter::execute() {
+    
+}
--- Interpreter/Interpreter.h
+++ Interpreter/Interpreter.h
@@ -1,1 +1,15 @@
 #pragma once
+
+#include "Params.h"
+#include "Tilemap.h"
+#include "Turtle.h"
+
+class Interpreter {
+    Params params;
+    TileMap tilemap;
+    Turtle turtle;
+
+  public:
+    Interpreter(Params &params);
+    bool execute();
+};
