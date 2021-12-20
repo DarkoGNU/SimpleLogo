e273b873df6efffeb088db538709cd79c08d5b00
DarkoGNU <42816979+DarkoGNU@users.noreply.github.com>
2021-12-20 17:20:12 +0100

    Project - it compiles without Lua

--- Interpreter/Interpreter.cpp
+++ Interpreter/Interpreter.cpp
@@ -2,11 +2,7 @@
 
 #include <algorithm>
 
-#include "LuaBridge/LuaBridge.h"
-#include "Parser.h"
 #include "Writer.h"
-
-namespace lb = luabridge;
 
 Interpreter::Interpreter(const int argc, char const *const argv[])
     : status(params.readParams(argc, argv)), tilemap(params.getSize()),
@@ -15,44 +11,6 @@ Interpreter::Interpreter(const int argc, char const *const argv[])
 bool Interpreter::bad() const { return !status; }
 
 bool Interpreter::execute() {
-    Parser parser(params.getInputPath());
-
-    if (!parser.parse()) {
-        status = false;
-        return false;
-    }
-
-    // Initialize the VM
-    std::unique_ptr<lua_State, lua_Closer> L(luaL_newstate());
-    luaL_openlibs(L.get());
-
-    // Register Turtle class and its functions
-    lb::getGlobalNamespace(L.get())
-        .beginClass<Turtle>("Turtle")
-        .addFunction("przod", &Turtle::forward)
-        .addFunction("tyl", &Turtle::back)
-        .addFunction("prawo", &Turtle::right)
-        .addFunction("lewo", &Turtle::left)
-        .endClass();
-
-    // Register turtle
-    lb::push(L.get(), &turtle);
-    lua_setglobal(L.get(), "turtle");
-
-    // Execute the code and check the result
-    if (luaL_dostring(L.get(), parser.getLua().c_str()) != LUA_OK) {
-        std::cerr << lua_tostring(L.get(),-1) << '\n';
-
-        status = false;
-        return false;
-    }
-
-    // Write the TileMap
-    if (!writeMap()) {
-        status = false;
-        return false;
-    }
-
     return true;
 }
 
--- Interpreter/Interpreter.h
+++ Interpreter/Interpreter.h
@@ -5,10 +5,8 @@
 #include "Params.h"
 #include "Tilemap.h"
 #include "Turtle.h"
-#include "lua.hpp"
 
 class Interpreter {
-    struct lua_Closer;
 
     Params params;
     bool status;
@@ -23,8 +21,4 @@ class Interpreter {
     bool bad() const;
 
     bool execute();
-};
-
-struct Interpreter::lua_Closer {
-    void operator()(lua_State *lua) { lua_close(lua); }
 };
