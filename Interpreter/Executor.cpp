0df898ca47e53feb24ae2cbbe8acc6b015586f2e
DarkoGNU <42816979+DarkoGNU@users.noreply.github.com>
2021-12-28 01:17:02 +0100

    Basic procedure calling logic

--- Interpreter/Executor.cpp
+++ Interpreter/Executor.cpp
@@ -13,16 +13,39 @@ std::unordered_map<std::string, unsigned int> Executor::createProcedureMap() {
 }
 
 void Executor::execute() {
-    call(code[0], std::unordered_map<std::string, double>());
+    auto nArgMap = std::unordered_map<std::string, double>();
+    call(code[0], nArgMap);
 }
 
 void Executor::call(std::vector<TurtleCommand> const &current,
-                    std::unordered_map<std::string, double> const &argMap) {
+                    std::unordered_map<std::string, double> &argMap) {
 
-    for (unsigned int i = 0; i < current.size(); i++);
-        //handleCommand(current[i], argMap, i);
+    for (unsigned int i = 1; i < current.size(); i++)
+        handleCommand(current[i], argMap, i);
 }
 
-void handleCommand(TurtleCommand const &current,
-                   std::unordered_map<std::string, double> const &argMap,
-                   unsigned int &pos) {}
+void Executor::handleCommand(TurtleCommand const &current,
+                             std::unordered_map<std::string, double> &argMap,
+                             unsigned int &pos) {
+
+    if (current.type == TurtleCommand::Type::call) {
+        auto index = procedureMap[current.name];
+        auto const &procedure = code[index];
+        auto const &definition = procedure[0];
+
+        auto map = getArgMap(definition, current, argMap);
+        call(procedure, map);
+    }
+}
+
+std::unordered_map<std::string, double>
+Executor::getArgMap(TurtleCommand const &definition,
+                    TurtleCommand const &current,
+                    std::unordered_map<std::string, double> &argMap) {
+
+    std::unordered_map<std::string, double> nArgMap;
+    for (unsigned int i = 0; i < current.args.size(); i++)
+        nArgMap[definition.args[i].name] = evaluateArg(current.args[i], argMap);
+
+    return nArgMap;
+}
--- Interpreter/Executor.hpp
+++ Interpreter/Executor.hpp
@@ -13,16 +13,20 @@ class Executor {
 
     // Warning - the order matters!
     const std::vector<std::vector<TurtleCommand>> code;
-    const std::unordered_map<std::string, unsigned int> procedureMap;
+    std::unordered_map<std::string, unsigned int> procedureMap;
 
     // Methods
     std::unordered_map<std::string, unsigned int> createProcedureMap();
     void call(std::vector<TurtleCommand> const &current,
-              std::unordered_map<std::string, double> const &argMap);
+              std::unordered_map<std::string, double> &argMap);
 
     void handleCommand(TurtleCommand const &current,
-                       std::unordered_map<std::string, double> const &argMap,
+                       std::unordered_map<std::string, double> &argMap,
                        unsigned int &pos);
+
+    std::unordered_map<std::string, double>
+    getArgMap(TurtleCommand const &definition, TurtleCommand const &current,
+              std::unordered_map<std::string, double> &argMap);
 
   public:
     Executor(std::vector<std::vector<TurtleCommand>> code, Turtle &turtle);
