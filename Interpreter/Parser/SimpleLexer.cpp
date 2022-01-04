316505499534d3677feb212285e1d9ee47a203bd
DarkoGNU <42816979+DarkoGNU@users.noreply.github.com>
2022-01-05 00:35:59 +0100

    Fix a buggy regex

--- Interpreter/Parser/SimpleLexer.cpp
+++ Interpreter/Parser/SimpleLexer.cpp
@@ -28,9 +28,10 @@ std::string cleanString(std::string const &text) {
         result += line;
     }
 
-    // Replace ) with );
-    const static std::regex pattern1{R"(\)[^;])"};
-    return regex_replace(result, pattern1, ");");
+    // Replace ) with );. Make sure that the string ends with whitespace
+    const static std::regex pattern1{R"(\)(?=[^;]))"};
+    return regex_replace(result + ' ', pattern1, ");");
+    // This will double ; in cases like ") ;", but tokenize will deal with it
 }
 } // namespace
 
